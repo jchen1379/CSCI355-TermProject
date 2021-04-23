4105529a3b3c5074227402d85dec5a567a7432b0
Jianhui Chen <55901916+jchen1379@users.noreply.github.com>
2021-04-22 21:56:42 -0400

    Add files via upload

---
+++ TermProject/createTable.sql
@@ -0,0 +1,43 @@
+CREATE TABLE `STUDENT` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(30) NOT NULL,
+  `lastName` varchar(30) NOT NULL,
+  `SSNLast4` varchar(4) DEFAULT NULL,
+  `Email` varchar(100) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=10000001 DEFAULT CHARSET=latin1
+
+
+
+CREATE TABLE `STAFF` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(30) NOT NULL,
+  `lastName` varchar(30) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `title` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=10000001 DEFAULT CHARSET=latin1
+
+
+
+CREATE TABLE `APPLICATION` (
+  `applicationID` int(11) NOT NULL AUTO_INCREMENT,
+  `requestTime` datetime DEFAULT NULL,
+  `applicationStatus` varchar(10) DEFAULT NULL,
+  `staffID` int(11) DEFAULT NULL,
+  `studentID` int(11) DEFAULT NULL,
+  `semester` varchar(10) DEFAULT NULL,
+  `year` int(11) DEFAULT NULL,
+  `career` varchar(20) DEFAULT NULL,
+  `reason` text DEFAULT NULL,
+  `phone` varchar(15) DEFAULT NULL,
+  `feedback` text DEFAULT NULL,
+  `processedTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`applicationID`),
+  KEY `staffID` (`staffID`),
+  KEY `studentID` (`studentID`),
+  CONSTRAINT `APPLICATION_ibfk_1` FOREIGN KEY (`staffID`) REFERENCES `STAFF` (`ID`),
+  CONSTRAINT `APPLICATION_ibfk_2` FOREIGN KEY (`studentID`) REFERENCES `STUDENT` (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=1000000001 DEFAULT CHARSET=latin1
